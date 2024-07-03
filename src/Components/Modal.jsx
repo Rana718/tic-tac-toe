1fa64b3b7744ea036e12d022038fe621f873dcf0
Rana718 <ranadolui718@gmail.com>
2024-07-03 16:38:13 +0530

    update

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = ({ message, onClose }) => {
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
-      <div className="bg-gradient-to-r from-yellow-500 to-blue-500 p-6 rounded-lg shadow-lg text-center text-white">
+      <div className="bg-gradient-to-r from-blue-200 to-purple-300  p-6 rounded-lg shadow-lg text-center text-white">
         <h2 className="text-2xl font-bold mb-4">{message}</h2>
         <button
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300"
