3c79db4025482f64bac651131a3f6311b15465f4
Rana718 <ranadolui718@gmail.com>
2024-07-31 19:49:47 +0530

    add play Again and Find new Player function

--- backend/online/online.js
+++ backend/online/online.js
@@ -2,6 +2,7 @@ module.exports = (io) => {
     let waitingPlayer = null;
     const restartRequests = {};
     const rooms = {};
+    const rematchTimers = {};
 
     io.on("connection", (socket) => {
         console.log("A user connected:", socket.id);
@@ -16,6 +17,7 @@ module.exports = (io) => {
             socket.join(roomID);
 
             io.to(roomID).emit("startGame", { roomID, players });
+            io.to(roomID).emit("resetBoard");
 
             rooms[roomID] = players;
             restartRequests[roomID] = { X: false, O: false };
@@ -35,34 +37,73 @@ module.exports = (io) => {
             restartRequests[roomID][player] = true;
 
             if (restartRequests[roomID].X && restartRequests[roomID].O) {
+                clearTimeout(rematchTimers[roomID]);
                 io.to(roomID).emit("resetBoard");
                 restartRequests[roomID] = { X: false, O: false };
+            } else {
+                io.to(roomID).emit("startTimer");
+
+                rematchTimers[roomID] = setTimeout(() => {
+                    if (!restartRequests[roomID].X || !restartRequests[roomID].O) {
+                        io.to(roomID).emit("gameOver", { message: "Sorry, I can't play more this time." });
+                        io.to(roomID).emit("showFindNewPlayerButton");
+                    }
+                }, 20000);
+            }
+        });
+
+        socket.on("findNewPlayer", () => {
+            if (waitingPlayer) {
+                const roomID = `${waitingPlayer.id}#${socket.id}`;
+                waitingPlayer.join(roomID);
+                socket.join(roomID);
+
+                const players = {
+                    [waitingPlayer.id]: "X",
+                    [socket.id]: "O"
+                };
+
+                io.to(roomID).emit("startGame", { roomID, players });
+                io.to(roomID).emit("resetBoard");
+
+                rooms[roomID] = players;
+                restartRequests[roomID] = { X: false, O: false };
+
+                waitingPlayer = null;
+            } else {
+                waitingPlayer = socket;
             }
         });
 
         socket.on("disconnect", () => {
             console.log("User disconnected:", socket.id);
+            const roomID = Object.keys(rooms).find(room => rooms[room][socket.id]);
+            if (roomID) {
+                socket.to(roomID).emit("userDisconnected");
+                delete rooms[roomID];
+                delete restartRequests[roomID];
+                clearTimeout(rematchTimers[roomID]);
+            }
             if (waitingPlayer === socket) {
                 waitingPlayer = null;
             }
         });
 
-        const notifyPlayers = (roomID, winnerPlayer, players) =>{
+        const notifyPlayers = (roomID, winnerPlayer, players) => {
             const winnerSocketId = Object.keys(players).find(id => players[id] === winnerPlayer);
             const loserSocketId = Object.keys(players).find(id => players[id] !== winnerPlayer);
             io.to(winnerSocketId).emit("gameOver", { message: "🎉 Congratulations! You’ve emerged victorious! 🏆" });
             io.to(loserSocketId).emit("gameOver", { message: "😔 Tough luck this time. Better luck next game!" });
-
-        }
+        };
 
         socket.on("gameOver", ({ roomID, winner }) => {
             const players = rooms[roomID];
-            if(!players){
+            if (!players) {
                 console.error(`Players not found for room ${roomID}`);
                 return;
             }
-            
+
             notifyPlayers(roomID, winner, players);
-        })
+        });
     });
 };
